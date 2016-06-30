e178a74cae2be56b423920d705208fc1ffdab8ed
renuka1 <renukajeeva97@gmail.com>
2016-06-30 15:27:54 +0530

    Create talking mickey.c

---
+++ talking mickey.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+  char  a[100];
+  printf("Enter your name :\n");
+  scanf("%s", a);
+  printf("Hello %s ! Welcome to Disneyland!!!",a);
+  return 0;
+}
