e35cc2e7752c31d38af514cce8dfe5fd00c06b64
renuka1 <renukajeeva97@gmail.com>
2016-06-30 16:15:04 +0530

    Create cardg11.c

---
+++ cardg11.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,j=0,i=0,c;
+  printf("Enter the card number picked by Patrick:\n");
+  scanf("%d",&a);
+   printf("Enter the card number picked by Johnny:\n");
+  scanf("%d",&b);
+ if((a>=0) && (b>=0))
+  {
+    while(a>0)
+    {
+    i=i*10;
+ i=i+(a%10);
+      a=a/10;
+  }
+    while(b>0)
+  {
+        j=j*10;
+ j=j+(b%10);
+      b=b/10;
+  }
+    c=i+j;
+   printf("Sum is %d",c);
+  } 
+  else if(a<0 && b<0)
+    {
+      i=-(a);
+      j=-(b);
+      c=i+j;
+      printf("Sum is %d",c);
+    }
+  else if(a<0)
+  {
+    while(b>0)
+        {
+        j=j*10;
+ j=j+(b%10);
+      b=b/10;
+  }
+    c=j-a;
+    printf("Sum is %d",c);
+  }
+    else
+    {
+      while(a>0)
+           {
+    i=i*10;
+ i=i+(a%10);
+      a=a/10;
+  }
+      c=i-b;
+    printf("Sum is %d",c);
+    }
+    
+  return 0;
+}
