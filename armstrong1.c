3817c2b58db79fbe06516d3605cd425334c98dd6
RenuJ <renukajeeva97@gmail.com>
2016-12-19 13:47:20 +0530

    Create armstrong1.c

---
+++ armstrong1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int l,h,i,t1,t2,rem,n=0,r=0;
+    scanf("%d %d",&l,&h);
+    printf("Armstrong numbers between %d an %d are: ",l,h);
+    for(i=l+1;i<h;++i)
+    {
+        t2=i;
+        t1=i;
+        while (t1!=0)
+        {
+            t1/=10;
+            ++n;
+        }
+        while (t2!=0)
+        {
+            rem = t2% 10;
+            r+= pow(rem,n);
+            t2/=10;
+        }
+        if (r==i) {
+            printf("%d ",i);
+        }
+        r=0;
+    }
+    return 0;
+}
