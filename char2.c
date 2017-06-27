ad215bd9427ee787d489a35ed5bcf41f7e840007
RenuJ <renukajeeva97@gmail.com>
2017-06-27 15:42:19 +0530

    Create char2.c

---
+++ char2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int c,k=1;
+char abc[100];
+clrscr();
+printf("Enter the sentence");
+scanf("%s",&abc);
+for(c=0;abc[c]!='\0';c++)
+{
+if(ch[i]!=' ')
+k=k+1;
+}
+printf("%d",k);
+getch();
+return 0;
+}
