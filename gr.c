d6ff7f58d097e1a646c2915ca1d9298f3e62300c
RenuJ <renukajeeva97@gmail.com>
2017-06-27 16:22:10 +0530

    Create gr.c

---
+++ gr.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include,conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+if(a>b)
+{
+if(a>c)
+{
+printf("A");
+}
+else if(b>c)
+{
+printf("B");
+}
+else
+{
+printf("C");
+}
+}
+getch();
+return 0;
+}
