69f5118a609d06feb07d2d6b6505ac91d920edc6
RenuJ <renukajeeva97@gmail.com>
2017-06-27 16:12:59 +0530

    Update palin.c

--- palin.c
+++ palin.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 int main()
 {
-   int n, rev=0, remr,t;
+   int n,n1,r,ans=0;
    scanf("%d", &n);
-   t=n;
-   while(t!=0)
+   n1=n;
+   while(n1!=0)
    {
-      rem=t%10;
-      rev=rev*10+rem;
-      t/=10;
+      r=n1%10;
+      ans=ans*10+r;
+      n1=n1/10;
    } 
-if(rev==n) 
-      printf("%d is a palindrome number",n);
+if(ans==n1) 
+      printf("%d is a palindrome number",n1);
    else
-      printf("%d is not a palindrome number",n);
+      printf("%d is not a palindrome number",n1);
    return 0;
 }
