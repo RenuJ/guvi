cd8c006ea153e60066bdc1c0e2e6b493c5a7b89e
RenuJ <renukajeeva97@gmail.com>
2017-06-28 13:58:26 +0530

    Create r2.c

---
+++ r2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int k;
+scanf("%d",&k);
+if(k>=50 && k<=100)
+printf("%d is in valid range",k);
+else
+printf("%d is not a valid range",k):
+getch();
+return 0;
+}
